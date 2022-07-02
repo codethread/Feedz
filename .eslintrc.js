ed92c08708dc96903d6f31fe65657b8de94b5441
Adam Hall <adamhalldesigns@gmail.com>
2022-07-02 14:40:47 +0100

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    'react-native/react-native': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'react-native'],
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'react/react-in-jsx-scope': 0,
+  },
+};
--- App.tsx
+++ App.tsx
@@ -1,1 +1,1 @@
-export { Main as default } from './src/Main'
+export { Main as default } from './src/Main';
